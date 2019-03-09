c73dcfe03655ca04bba81b05696e60f3301dc384
beaver <none>
2019-03-09 22:14:55 +0800

    Add read timeout and retry.

--- qiquge.py
+++ qiquge.py
@@ -4,7 +4,7 @@
 import urllib.request
 import ssl
 import socket
-from bs4 import BeautifulSoup
+
 import codecs
 
 from lxml import etree
@@ -17,46 +17,55 @@ url = "https://www.qu.la"
 headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/72.0.3626.109 Safari/537.36"}
 
-req = urllib.request.Request(url=url, headers=headers)
-with urllib.request.urlopen(req) as uf:
-    hx = etree.HTML(uf.read())
-    booklist = hx.xpath('//div[@id="hotcontent"]//div[@class="image"]//@href')
+total = 0
 
-    for b in booklist[:1]:
-        burl = url + b
-        req = urllib.request.Request(url=burl, headers=headers)
-        with urllib.request.urlopen(req) as buf:
-            dom = etree.HTML(buf.read())
-            div = dom.xpath('//div[@class="box_con"]')[1]
-            al = div.xpath('//dd/a')
-            for a in al:
-                curl = a.xpath('./@href')[0]
-                if not curl.startswith('/book'):
-                    continue
-                curl = url + curl
-                cn = a.xpath('./text()')[0]
 
-                print(cn + ": " + curl)
-                req = urllib.request.Request(url=curl, headers=headers)
-                with urllib.request.urlopen(req, timeout=5) as cuf:
-                    try:
-                        dom = etree.HTML(cuf.read())
-                    except (socket.timeout, urllib.error.URLError):
-                        continue
+def get_page_xpath(url):
+    request = urllib.request.Request(url=url, headers=headers)
+    while True:
+        try:
+            with urllib.request.urlopen(request, timeout=5) as f:
+                html = f.read()
+                break
+        except urllib.error.URLError:
+            print("URLError:try again: " + url)
+            continue
+        except socket.timeout:
+            print("read timeout: again: " + url)
+            continue
+    return etree.HTML(html)
 
-                    # write chapter
-                    file.write(cn.encode('utf-8').decode().strip() + "\n")
-                    content = dom.xpath('//div[@id="content"]/text()')
-                    for c in content:
-                        c = c.encode('utf-8').decode().strip()
-                        if len(c) is 0:
-                            continue
-                        elif len(c) > 0:
-                            c += '\n'
-                        file.write(c)
-                    file.write("\n\n\n")
 
-            file.close()
+main_page = get_page_xpath(url)
+booklist = main_page.xpath('//div[@id="hotcontent"]//div[@class="image"]//@href')
+for b in booklist[:1]:
+    burl = url + b
+    book_page = get_page_xpath(burl)
+    div = book_page.xpath('//div[@class="box_con"]')[1]
+    al = div.xpath('//dd/a')
+    for a in al:
+        curl = a.xpath('./@href')[0]
+        if not curl.startswith('/book'):
+            continue
+        curl = url + curl
+        cn = a.xpath('./text()')[0]
+
+        print(cn + ": " + curl)
+        chapter_page = get_page_xpath(curl)
+        file.write(cn.encode('utf-8').decode().strip() + "\n")
+        content = chapter_page.xpath('//div[@id="content"]/text()')
+        for c in content:
+            c = c.encode('utf-8').decode().strip()
+            if len(c) is 0:
+                continue
+            elif len(c) > 0:
+                c += '\n'
+            file.write(c)
+        file.write("\n\n\n")
+        total += 1
+        print("count: " + str(total) + cn + ": scratch success")
+
+    file.close()
 
 
 
