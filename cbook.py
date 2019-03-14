fcf6eaabd12f366c37c778f576cbe12045030eef
jack <hjj414@gmail.com>
2019-03-14 21:41:52 +0800

    ...

--- cbook.py
+++ cbook.py
@@ -16,7 +16,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/72.0.3626.109 Safari/537.36"}
 logging.basicConfig(format="%(asctime)s: <%(thread)d> [%(levelname)s] %(message)s",
-                    level=logging.DEBUG)
+                    level=logging.DEBUG,
+                    filename="download-booklist.log")
 
 
 def get_html(url, retries=10):
